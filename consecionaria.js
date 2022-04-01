187e22ae5f3e3a6ea5aa812535709df39f34a13f
ccFabrissin <cayefa@gmail.com>
2022-03-31 22:14:21 -0300

    ahora

---
+++ consecionaria.js
@@ -0,0 +1,12 @@
+const autos = require("./autos");
+
+
+const concesionaria = {
+   autos: autos,
+   buscarAuto: autos.filter(function(patente){
+           return patente == this.patente;
+       })
+   }
+;
+
+console.log(concesionaria.buscarAuto);
